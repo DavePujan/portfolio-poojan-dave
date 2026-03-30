5f7e0ae98a88912b0970d248f7664995ca8d5cf7
Poojan Dave <poojandave0506@gmail.com>
2026-03-31 02:50:27 +0530

    feat: Add analytics tracking and dynamic section rendering with scene state

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
 import { AnimatePresence } from 'framer-motion'
+import { useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 import AppRoutes from './routes'
+import RootLayout from './components/layout/RootLayout'
+import usePerformanceMode from './hooks/usePerformanceMode'
+import { trackEvent, trackPageView } from './utils/analytics'
 
 export default function App() {
   const location = useLocation()
+  const performance = usePerformanceMode()
+
+  useEffect(() => {
+    trackPageView(location.pathname)
+  }, [location.pathname])
+
+  useEffect(() => {
+    trackEvent('performance_mode_detected', {
+      mode: performance.mode,
+      lowPerf: performance.lowPerf,
+    })
+  }, [performance.lowPerf, performance.mode])
 
   return (
-    <AnimatePresence mode="wait">
-      <AppRoutes key={location.pathname} location={location} />
-    </AnimatePresence>
+    <RootLayout>
+      <AnimatePresence mode="wait">
+        <AppRoutes key={location.pathname} location={location} />
+      </AnimatePresence>
+    </RootLayout>
   )
 }
